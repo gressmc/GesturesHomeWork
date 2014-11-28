4b7fe60417a4b2d549d5edf8bd309a78f9bb28a8
gressmc <gressmc@iMac.local>
2014-11-28 18:57:34 +0300

    Initial Commit

---
+++ GesturesHomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturesHomeWork
+//
+//  Created by gressmc on 28/11/14.
+//  Copyright (c) 2014 gressmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
